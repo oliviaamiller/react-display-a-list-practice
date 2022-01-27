f4a57662d714328c48a0a7046cc5f73882f128b2
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-27 11:07:14 -0800

    passed props to sweaterlist, mapped through sweaters array

--- src/Candies/CandyList.js
+++ src/Candies/CandyList.js
@@ -4,7 +4,7 @@ export default function CandyList(props) {
   return <div className='candy-list'>
     { 
       props.candies.map((candy, i) =>
-        <Candy key ={`${candy}-${i}`}
+        <Candy key={`${candy}-${i}`}
           candy={candy}/>)
     }
   </div>;
--- src/Sweaters/SweaterList.js
+++ src/Sweaters/SweaterList.js
@@ -1,1 +1,13 @@
+import Sweater from './Sweater';
+
+export default function SweaterList(props) {
+  return <div className='sweater-list'>
+    {
+      props.sweater.map((sweater, i) => 
+        <Sweater key={`${sweater.fiber}-${i}`} 
+          {...sweater}/>)
+    }
+
+  </div>;
+}
 
