e0f6710c5753f4d3baa0561293c31298fb21aed4
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-27 11:15:17 -0800

    imported cats array and cats list to app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { candies } from './Candies/candies-data';
 import CandyList from './Candies/CandyList';
 import { sweaters } from './Sweaters/sweater-data';
 import SweaterList from './Sweaters/SweaterList';
+import { cats } from './Cats/cats-data';
+import CatsList from './Cats/CatsList';
 // import your arrays here
 
 function App() {
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
-      <CandyList candies={candies}/>
-      <SweaterList sweaters={sweaters}/>
+      <CandyList candies={candies} />
+      <SweaterList sweaters={sweaters} />
+      <CatsList cats={cats} />
     </div>
   );
 }
