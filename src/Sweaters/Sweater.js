25631a126dd42b35cc48cbabe71d3e83b12ed1f0
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-27 14:35:55 -0800

    sweater css styles added

--- src/Sweaters/Sweater.js
+++ src/Sweaters/Sweater.js
@@ -1,7 +1,8 @@
 import './Sweaters.css';
 
-export default function Sweater({ fiber, style, timesWorn }) {
+export default function Sweater({ name, fiber, style, timesWorn }) {
   return <div className='sweater'>
+    <p className='sweater-name'>{name}</p>
     <p>fiber: {fiber}</p>
     <p>style: {style}</p>
     <p>times worn: {timesWorn}</p>
--- src/Sweaters/sweater-data.js
+++ src/Sweaters/sweater-data.js
@@ -1,15 +1,18 @@
 export const sweaters = [
-  {   
+  {  
+    name: 'No. 1',
     fiber: 'merino',
     style: 'turtleneck',
     timesWorn: 20
   },
   {
+    name: 'No. 2',
     fiber: 'cashmere',
     style: 'tunic',
     timesWorn: 12
   },
   {
+    name: 'No. 3',
     fiber: 'wool',
     style: 'cardigan',
     timesWorn: 50
