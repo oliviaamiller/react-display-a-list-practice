5a01b70fd490fd5f4bdf80d797ba4e785c9b30ed
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-27 11:02:49 -0800

    imported sweaterdata and sweaterlist to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { candies } from './Candies/candies-data';
 import CandyList from './Candies/CandyList';
+import { sweaters } from './Sweaters/sweater-data';
+import SweaterList from './Sweaters/SweaterList';
 // import your arrays here
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
         Render all your lists here. Pass the arrays as props.
       <CandyList candies={candies}/>
+      <SweaterList sweaters={sweaters}/>
     </div>
   );
 }
---
+++ src/Sweaters/sweater-data.js
@@ -0,0 +1,17 @@
+export const sweaters = [
+  {   
+    fiber: 'merino',
+    style: 'turtleneck',
+    timesWorn: 20
+  },
+  {
+    fiber: 'cashmere',
+    style: 'tunic',
+    timesWorn: 12
+  },
+  {
+    fiber: 'wool',
+    style: 'cardigan',
+    timesWorn: 50
+  }
+];
