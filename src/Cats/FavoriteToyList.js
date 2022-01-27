f7db58a433ad19106a818639c13e644150d006b6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-27 11:58:39 -0800

    mapped favorite toy array

--- src/Cats/FavoriteToyList.js
+++ src/Cats/FavoriteToyList.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import FavoriteToy from './FavoriteToy';
 
-export default function FavoriteToyList() {
-  return <div></div>;
+export default function FavoriteToyList({ favoriteToy }) {
+  return <div className='favorite-toy-list'>
+    { favoriteToy.map((toy, i) =>
+      <FavoriteToy key={`${toy.type}-${i}`}
+        {...toy} />) }
+  </div>;
 }
